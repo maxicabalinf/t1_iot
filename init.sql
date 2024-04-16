a72db0d24320a532f420dbb5c687b5429e3d08da
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2024-04-15 22:54:43 -0400

    refactor: renombra script sql

--- init.sql
+++ init.sql
@@ -1,9 +1,22 @@
 CREATE TABLE transport_layer (
   id SERIAL PRIMARY KEY,
-  name VARCHAR NOT NULL
+  name_ VARCHAR (3) NOT NULL
 );
 
-CREATE TABLE device (id SERIAL PRIMARY KEY, mac MACADDR);
+CREATE TABLE device (
+  id SERIAL PRIMARY KEY,
+  mac MACADDR,
+  UNIQUE (id, mac)
+);
+
+CREATE TABLE body_protocol (
+  id SERIAL PRIMARY key,
+  has_timestamp BOOLEAN,
+  has_thpc BOOLEAN,
+  has_acc_kpi BOOLEAN,
+  has_acc_sensor BOOLEAN
+  -- n_atts INTEGER
+);
 
 CREATE TABLE log_entry (
   device_id INTEGER,
@@ -21,23 +34,14 @@ CREATE TABLE datum (
   delta_time TIME,
   packet_loss INTEGER,
   PRIMARY KEY (device_id, timestamp_),
-  FOREIGN KEY (device_id, device_mac) REFERENCES device(id, mac),
+  FOREIGN KEY (device_id, device_mac) REFERENCES device(id, mac)
 );
 
-CREATE TABLE body_protocol (
-  id SERIAL PRIMARY key,
-  has_timestamp BOOLEAN,
-  has_thpc BOOLEAN,
-  has_kpi BOOLEAN,
-  has_sensor BOOLEAN,
-  -- n_atts INTEGER
-);
 
 CREATE TABLE configuration (
   body_protocol_id INTEGER,
   transport_layer_id INTEGER,
-  one_row_only_uidx BOOLEAN DEFAULT 1,
-  PRIMARY KEY (device_id, timestamp_),
-  UNIQUE one_row_only_uidx,
-  CHECK (one_row_only_uidx = 1)
+  one_row_only_uidx BOOLEAN UNIQUE DEFAULT 't', -- Asegura única fila
+  PRIMARY KEY (body_protocol_id, transport_layer_id),
+  CHECK (one_row_only_uidx = 't') -- Asegura única fila
 );
