a4a69a58e1dbff182f9d69b624357606e48e4d02
Maximiliano Cabalín <maxi.cabalin.f@gmail.com>
2024-04-21 16:20:09 -0400

    feat(server): esbozos de flujo

--- codigo_rasp/server.py
+++ codigo_rasp/server.py
@@ -1,4 +1,3 @@
-import socket
 """"
 HOST = '0.0.0.0'  # Escucha en todas las interfaces disponibles
 PORT = 1234       # Puerto en el que se escucha
@@ -22,16 +21,95 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 conn.sendall(respuesta.encode('utf-8'))
 """
 
-## Consultar la tabla de configuración en la base de datos
-## Crear 2 sockets, TCP Y UDP
-## while infinito revisando la base de datos.(siempre comienza con TCP)
-## Parsear los datos
-## Guardarlos en la base de datos
+# import socket
+import jsockets
+import sys
+from modelos import Configuration
 
-## Si el largo del body es menor que de los headers, entonces se calcula
-## la diferencia entre el largo indicado en los headers y lo que llegó.
- 
- ## Se almacena la diferencia de timestamps entre que se guardó la conexión en la tabla Logs 
-## y el servidor escribió los datos en la tabla Datos.
 
-## Finalmente se reinicia el ciclo.
+def get_cfg():
+    """Extrae la configuración de la base de datos"""
+    # data_list = []
+    # cfg = Configuration.select()[0]
+    return cfg
+
+
+print(get_cfg())
+
+
+def build_headers(id_msg, mac, transport_layer, id_protocol, length):
+    """Retorna el header de un mensaje"""
+    bytes_list = [
+        id_msg.to_bytes(2),
+        mac.to_bytes(6),
+        transport_layer.to_bytes(1),
+        id_protocol.to_bytes(1),
+        length.to_bytes(2),
+    ]
+
+    header = b''.join(bytes_list)
+    return header
+
+
+# HOST = '0.0.0.0'  # Escucha en todas las interfaces disponibles
+PORT = 1234       # Puerto en el que se escucha
+
+s = jsockets.socket_tcp_bind(PORT)  # Abre socket y queda a la escucha
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+# Esperando un mensaje con TCP
+while True:
+    # Acepta una conexión de ESP
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    header_ini = conn.recv(1024)
+    # Consultar la tabla de configuración en la base de datos
+    cfg = get_cfg()
+    response_header = """"""
+    conn.send(response_header)
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.send(data)
+    # conn.close()
+    # print('Client disconnected')
+
+
+def send_headers(sock):
+    # Consulta BD para obtener configuración
+    # Crea headers
+    # Envía headers mediante el socket entregado
+    pass
+
+
+def parse_msg(pkt):
+    """Deconstruye el paquete."""
+    pass
+
+
+# Espera un mensaje y protocolo de acuerdo  a lo que consulto en la BD.
+# Parsear y guardar en la base de datos.
+packet: bytes = None
+parse_msg(packet)
+
+
+res: bytes = None  # solicitud de conexión
+sock: socket = None
+send_headers(sock)
+
+
+# Crear 2 sockets, TCP Y UDP
+# while infinito revisando la base de datos.(siempre comienza con TCP)
+# Parsear los datos
+# Guardarlos en la base de datos
+
+# Si el largo del body es menor que de los headers, entonces se calcula
+# la diferencia entre el largo indicado en los headers y lo que llegó.
+
+# Se almacena la diferencia de timestamps entre que se guardó la conexión en la tabla Logs
+# y el servidor escribió los datos en la tabla Datos.
+
+# Finalmente se reinicia el ciclo.
